d6204b60a4ddfa09486e019b83f1ef90f7c7278c
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2018-12-21 14:59:28 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# RyanNHG
+> My personal site, built with Elm!
+
+### Local Development
+
+1. __`npm install`__
+
+1. __`npm run dev`__
---
+++ src/Main.elm
@@ -0,0 +1,9 @@
+module Main exposing (main)
+
+import Html exposing (text)
+
+
+main =
+    h1 []
+        [ text "Hello from Elm!"
+        ]
